16f1d5423b85a3e55444c693eb5116b0842c63eb
Charles Rocabert <charles.rocabert@gmail.com>
2019-12-21 10:55:52 -0800

    Python package case sensitivity debugging

--- metevolsim/__init__.py
+++ metevolsim/__init__.py
@@ -24,4 +24,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #***************************************************************************
 
-from .MetEvolSim import *
+from .metevolsim import *
